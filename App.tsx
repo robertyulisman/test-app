af06bb025caa2e9690de66f4eed0551c2fcb13c7
robertyulisman <desainku1092@gmail.com>
2024-10-25 14:38:05 +0700

    first init

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
