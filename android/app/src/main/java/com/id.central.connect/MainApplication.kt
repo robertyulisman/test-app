c48f205eefb468fc4a953e5b050fe763a33acadd
robertyulisman <desainku1092@gmail.com>
2024-11-22 22:57:02 +0700

    success install depedencies

--- android/app/src/main/java/com/id.central.connect/MainApplication.kt
+++ android/app/src/main/java/com/id.central.connect/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.microsoft.codepush.react.CodePush
 
 class MainApplication : Application(), ReactApplication {
 
@@ -22,6 +23,9 @@ class MainApplication : Application(), ReactApplication {
               // add(MyReactNativePackage())
             }
 
+        override fun getJSBundleFile(): String {
+                  return CodePush.getJSBundleFile()
+                }
         override fun getJSMainModuleName(): String = "index"
 
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
